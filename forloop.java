a3a55f67434566e53d7801760f9bb077bd12b2b8
divya80961141-blip <divya809611@gmali.com>
2026-07-11 17:05:09 +0530

    add forloop files

---
+++ forloop.java
@@ -0,0 +1,15 @@
+public class  forloop
+{
+    
+public static void main(String[] args)
+{
+    int i;
+    for(i = 10; i >=1; i--)
+    {
+    System.out.println(i);
+    }
+}
+} 
+
+
+
---
+++ loop1.java
@@ -0,0 +1,11 @@
+public class loop1
+{
+  public static void main(String[] agrs)
+{
+  int i=1; 
+  while(i <= 10){
+      i++;
+  System.out.println(i);
+}
+}
+}
