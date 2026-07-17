9c002fe097875f1baee868eb3f8031fa4a9f52f9
divya80961141-blip <290410240+divya80961141-blip@users.noreply.github.com>
2026-07-17 18:52:25 +0530

    ADD student.java

---
+++ .vscode/student.java
@@ -0,0 +1,34 @@
+public  class student{
+
+    String  Name;
+
+    int age;
+
+     student(){
+      Name= "divya";
+      age = 19;
+     }
+
+    void Name()
+    {
+        System.out.println(Name);
+    }
+    void age(){
+        System.out.println(age);
+    }
+    public static void main(String[] args){
+        student student1= new student();
+
+        student1.Name();
+        student1.age();
+    }
+}
+    
+
+
+
+
+
+
+
+
--- multiplicatiofor.java
+++ multiplicatiofor.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
 {
     int n=5;
     for(int i=1; i<=10;i++){
-    System.out.println(n +" x " + i + " =" +(n * 1));
+    System.out.println(n + " * " + i + " =  " +(n*i));
     }
 }
     }
