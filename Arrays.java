02e3085802e1273a92e15f16f37ffe017f9bc69a
divya80961141-blip <290410240+divya80961141-blip@users.noreply.github.com>
2026-07-18 22:05:46 +0530

    ArddArrayfiles

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,4 @@
-public class Arryas 
+public class Arrays
 { 
     public static void main(String[] args) 
     {
---
+++ Arrays1.java
@@ -0,0 +1,13 @@
+public class Arrays1 {
+    
+public static void main(String[] args){
+    int [] i=new int [] {10,20,30,40,50};
+    System.out.println(i[0]);
+    System.out.println(i[1]);
+    System.out.println(i[2]);
+    System.out.println(i[3]);
+    System.out.println(i[4]);
+    
+
+}
+}
