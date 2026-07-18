037a79b61fca522c8d7ac7617a1f7352b90550e8
divya80961141-blip <290410240+divya80961141-blip@users.noreply.github.com>
2026-07-18 22:25:10 +0530

    addarray2files

---
+++ Arrays2.java
@@ -0,0 +1,14 @@
+public class Arrays2 {
+    
+public static void main(String[] args) {
+    int[]i={30,40,50,60,70,80,90};
+    System.out.println(i[0]);
+    System.out.println(i[1]);
+    System.out.println(i[2]);
+    System.out.println(i[3]);
+    System.out.println(i[4]);
+    System.out.println(i[5]);
+    System.out.println(i[6]);
+    
+}
+}
